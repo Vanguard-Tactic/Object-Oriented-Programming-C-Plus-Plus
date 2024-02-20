0099d4351cd27f2994567b930b5a816a195c0af6
Vanguard-Tactic <157762395+Vanguard-Tactic@users.noreply.github.com>
2024-02-19 23:00:19 -0800

    Add files via upload

---
+++ Lab 2/Lab2_calc.cpp
@@ -0,0 +1,17 @@
+//Lab2_num_obj.cpp
+
+//This Lab will ...
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    //numerical 
+    int int_x = 1;
+    int int_y = 2;
+    double double_z = 2.0;
+
+    cout << "int divided by int " << int_x/int_y << endl;
+    cout << "double divided by double " << int_x/double_z << endl;
+}
